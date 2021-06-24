f2fa35480a53f56ad7f8075cff53f9c50ee90043
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2021-06-25 01:03:37 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState(" ");
+  const [heading, setHeading] = useState(" ");
+  function HandleOnchange(event) {
+    setName(event.target.value);
+  }
+
+  function HandleOnClick(event) {
+    setHeading(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {heading}</h1>
+      <form onSubmit={HandleOnClick}>
+        <input
+          onChange={HandleOnchange}
+          type="text"
+          placeholder="What's your name?"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
